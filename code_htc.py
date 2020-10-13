4d3418017b07e5e129e105f69af08c1590059224
Lohith Muppala <m.lohithrao@gmail.com>
2020-10-12 20:03:47 -0500

    Update code_htc.py
    Added the print function for the user to align the controller

--- code_htc.py
+++ code_htc.py
@@ -10,6 +10,39 @@ import sys
 import arm
 import os
 
+def print_cords(v):
+    """
+    This function prints the coordinates collected from the HTC Vive
+
+    Parameters
+    ----------
+    v : OpenVR obj
+        Instantiates an openVR object
+
+    Returns
+    -------
+    None. But prints the co-ordinates. 
+
+    """
+    if len(sys.argv) == 1:
+        interval = 1/250
+    elif len(sys.argv) == 2:
+        interval = 1/float(sys.argv[1])
+    else:
+        print("Invalid number of arguments")
+        interval = False
+        
+    if interval:
+        while(True):
+            start = time.time()
+            txt = ""
+            for each in v.devices["controller_1"].get_pose_euler():
+                txt += "%.4f" % each
+                txt += " "
+            print("\r" + txt, end="")
+            sleep_time = interval-(time.time()-start)
+            if sleep_time>0:
+                time.sleep(sleep_time)
 #st_robot is taken out
 def teaching_mode(v,obj,f):
     # """
@@ -150,7 +183,8 @@ def main():
     filename = os.path.join(final_directory, "coords" + str(num_paths) + ".txt")   
     f = open(filename, "w+") #collects the co-ords in a text file.
 
-    #prompt the user to get alligned with the base station
+    #prompt the user to get alligned with the base station -DONE??
+    print_cords(v) 
     #have a function to display the co-ordinates so that the user can align to the co-ords. 
     #TODO: How do I know if the switch is on or off like the toggle?
     #TODO: Create specific scneraios for the robot using different buttons: 1) Gripping 2) fine grain.
